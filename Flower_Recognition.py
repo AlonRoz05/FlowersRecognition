f6ea725e9f486e5c408d52799e1c16913755e347
AlonRoz <87911114+AlonRoz05@users.noreply.github.com>
2023-03-07 14:48:06 +0200

    Big update
    I made a cnn model that can recognize flowers. You can plot his predictions and see how good he is, you can also plot a confusion matrix.

--- Flower_Recognition.py
+++ Flower_Recognition.py
@@ -5,5 +5,113 @@ from torchvision import datasets, transforms
 from timeit import default_timer as timer
 from pathlib import Path
 from PIL import Image
+import helper_FRFN as helper
 import random
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+img_path = Path("Data")
+train_dir = img_path / "Training_Data"
+test_dir = img_path / "Testing_Data"
+
+img_path_list = list(img_path.glob("*/*/*.jpg"))
+random_img_path = random.choice(img_path_list)
+img_class = random_img_path.parent.stem
+
+img = Image.open(random_img_path)
+
+data_transform_train = transforms.Compose([
+    transforms.Resize(size=(64, 64)),
+    transforms.TrivialAugmentWide(num_magnitude_bins=31),
+    transforms.ToTensor()
+])
+
+data_transform_test = transforms.Compose([
+    transforms.Resize(size=(64, 64)),
+    transforms.ToTensor()
+])
+
+train_dataset = datasets.ImageFolder(root=train_dir, transform=data_transform_train)
+test_dataset = datasets.ImageFolder(root=test_dir, transform=data_transform_test)
+
+BATCH_SIZE = 32
+class_names = train_dataset.classes
+
+train_dataloader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+test_dataloader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+class Flowers_NN(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*13*13, out_features=output_shape)
+        )
+
+    def forward(self, x):
+        return self.classifier(self.conv_block_2(self.conv_block_1(x)))
+
+model = Flowers_NN(input_shape=3, hidden_units=10, output_shape=len(class_names))
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
+
+epochs = 5
+train = True
+show_model_quality = True
+if train:
+    start_training_time = timer()
+
+    model_results = helper.train_model(model=model, 
+                                        train_dataloader=train_dataloader,
+                                        test_dataloader=test_dataloader,
+                                        loss_fn=loss_fn, 
+                                        optimizer=optimizer,
+                                        epochs=epochs,
+                                        device=device)
+
+    end_training_time = timer()
+    print(f"Training took {end_training_time-start_training_time:.3f} on {device}")
+    helper.plot_loss_curves(results=model_results, epochs=epochs)
+
+    MODEL_PATH = Path("./models")
+    MODEL_PATH.mkdir(parents=True, exist_ok=True)
+    MODEL_NAME = "Flower_Detection_model.pth"
+    MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+    torch.save(model.state_dict(), MODEL_SAVE_PATH)
+
+else:
+    loaded_model = Flowers_NN(input_shape=3, hidden_units=10, output_shape=len(class_names))
+    loaded_model.load_state_dict(torch.load("./models/Flower_Detection_model.pth"))
+    loaded_model.to(device)
+
+    if show_model_quality:
+        test_samples = []
+        test_labels = []
+        for sample, label in random.sample(list(test_dataset), k=9):
+            test_samples.append(sample)
+            test_labels.append(label)
+
+        pred_probs = helper.make_predictions(model=loaded_model, data=test_samples, device=device)
+        pred_classes = pred_probs.argmax(dim=1)
+
+        helper.plot_model_predictions(pred_classes=pred_classes, test_labels=test_labels, test_samples=test_samples, class_names=class_names)
+        helper.p_confusion_matrix(model=loaded_model, test_data_loader=test_dataloader,data=test_dataset, class_names=class_names, device=device)
+
+    else:
+        print("in progress...")
 
---
+++ helper_FRFN.py
@@ -0,0 +1,145 @@
+import torch
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+from torchmetrics import ConfusionMatrix
+from mlxtend.plotting import plot_confusion_matrix
+from typing import List
+
+def accuracy_fn(y_true, y_pred):
+    """Returns the accuracy of the given y_true and y_pred values"""
+    correct = torch.eq(y_true, y_pred).sum().item()
+    accuracy = (correct / len(y_true)) * 100
+    return accuracy
+
+def train_step(model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, optimizer: torch.optim.Optimizer, device):
+    """Trains a model on a given data loader"""
+    train_loss, train_acc = 0, 0
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss  
+        train_acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    train_loss /= len(dataloader)
+    train_acc /= len(dataloader)
+    return train_loss, train_acc
+
+def test_step(model: torch.nn.Module, test_dataloader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, device):
+    """Tests how good the model is trained"""
+    test_loss, test_accuracy = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for batch, (X, y) in enumerate(test_dataloader):
+            X, y = X.to(device), y.to(device)
+            test_pred = model(X)
+
+            test_loss += loss_fn(test_pred, y).item()
+            test_accuracy += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+
+        test_loss /= len(test_dataloader)
+        test_accuracy /= len(test_dataloader)
+        return test_loss, test_accuracy
+
+def train_model(model: torch.nn.Module, train_dataloader: torch.utils.data.DataLoader, test_dataloader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, optimizer: torch.optim.Optimizer, epochs: int, device):
+    """Training and testing the model and returns the results of the training and testing loos and accuracy to plot loss and accuracy curves."""
+    results = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
+
+    for epoch in tqdm(range(epochs), leave=False):
+        train_loss, train_acc = train_step(model=model, dataloader=train_dataloader, loss_fn=loss_fn, optimizer=optimizer, device=device)
+        test_loss, test_acc = test_step(model=model, test_dataloader=test_dataloader, loss_fn=loss_fn, device=device)
+
+        print(f"Epoch: {epoch}\n--------------------------------")
+        print(f"Train loss: {train_loss:.4f} | Train accuracy: {train_acc:.2f}\nTest loss: {test_loss:.4f} | Test accuracy: {test_acc:.2f}\n--------------------------------")
+
+        results["train_loss"].append(train_loss.item())
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    return results
+
+def make_predictions(model: torch.nn.Module, data: list, device):
+    """doing predictions on 9 images from dataset"""
+    pred_probs = []
+    model.to(device)
+    model.eval()
+    with torch.inference_mode():
+        for sample in data:
+            sample = torch.unsqueeze(sample, dim=0).to(device)
+
+            pred_logits = model(sample)
+            pred_prob = torch.softmax(pred_logits.squeeze(), dim=0)
+            pred_probs.append(pred_prob.cpu())
+
+    return torch.stack(pred_probs)
+
+def plot_model_predictions(pred_classes, test_labels, test_samples, class_names):
+    """Plots the predictions from test_samples"""
+    plt.figure(figsize=(9,9))
+    nrows = 3
+    ncols = 3
+    for i, sample in enumerate(test_samples):
+        plt.subplot(nrows, ncols, i+1)
+        plt.imshow(sample.squeeze(), cmap="gray")
+        plt.axis(False)
+
+        pred_label = class_names[pred_classes[i]]
+        truth_label = class_names[test_labels[i]]
+
+        title_text = f"Pred {pred_label} | Truth {truth_label}"
+        if pred_label == truth_label:
+            plt.title(title_text, fontsize=10, c="g")
+        else:
+            plt.title(title_text, fontsize=10, c="r")
+
+    plt.show()
+
+def p_confusion_matrix(model: torch.nn.Module, test_data_loader: torch.utils.data.DataLoader, data, class_names, device):
+    """Plots confusion matrix"""
+    y_preds = []
+    model.eval()
+    with torch.inference_mode():
+        for X, y in tqdm(test_data_loader, desc="Making predictions..."):
+            X, y = X.to(device), y.to(device)
+            y_logit = model(X)
+            y_pred = torch.softmax(y_logit.squeeze(), dim=0).argmax(dim=1)
+            y_preds.append(y_pred.cpu())
+
+    y_pred_tensor = torch.cat(y_preds)
+    confmat = ConfusionMatrix(num_classes=len(class_names), task="multiclass")
+    confmat_tensor = confmat(preds=y_pred_tensor, target=data.targets)
+
+    fig, ax = plot_confusion_matrix(conf_mat=confmat_tensor.numpy(), class_names=class_names, figsize=(10,7))
+    plt.show()
+
+def plot_loss_curves(results: dict[str, List[float]], epochs: int):
+    """Plots Training curves for results dictionary"""
+    loss = results["train_loss"]
+    test_loss = results["test_loss"]
+    accuracy = results["train_acc"]
+    test_accuracy = results["test_acc"]
+
+    epochs = range(len(results["train_loss"]))
+
+    plt.figure(figsize=(15, 7))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label="train loss")
+    plt.plot(epochs, test_loss, label="test loss")
+    plt.title("Loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label="train accuracy")
+    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.title("Accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
+    plt.show()
+
