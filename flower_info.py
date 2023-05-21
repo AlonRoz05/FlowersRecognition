1f5bc32140a0587285bd1b7b29416a4a8c1a986d
AlonRoz <87911114+AlonRoz05@users.noreply.github.com>
2023-05-21 17:49:57 +0300

    Better code

--- flower_info.py
+++ flower_info.py
@@ -6,25 +6,25 @@ class_names = model_builder.class_names
 
 def get_flower_info(flower_img_path: str):
     models_choice, model_confidence = model_builder.get_image_data(image_path=flower_img_path)
-    
+
     if model_confidence == 100:
         model_confidence = 99.99
 
+    flower_name = class_names[models_choice]
     if models_choice == "Unknown":
         flower_name = "Unknown"
-    else:
-        flower_name = class_names[models_choice]
-
-    if flower_name == "Unknown":
         return "Unknown", "Unknown", "Unknown", "No Wikipedia page available for this flower.", f"{model_confidence:.2f}%"
-    else:
-        page = wiki.page(flower_name)
 
-        flower_family = ""
-        flower_information = page.summary
-        flower_page_url = page.fullurl
+    page = wiki.page(flower_name)
 
-        if flower_information[250:251]:
-            flower_information = f"{flower_information[0:250]}..."
+    flower_family = ""
+    flower_information = page.summary
+    flower_page_url = page.fullurl
 
-        return flower_name, flower_family, flower_information, flower_page_url, f"{model_confidence:.2f}%"
+    if flower_information[250:251]:
+        flower_information = f"{flower_information[0:250]}..."
+
+    if flower_information.find("()") != -1:
+        flower_information = flower_information.replace("()", "")
+
+    return flower_name, flower_family, flower_information, flower_page_url, f"{model_confidence:.2f}%"
